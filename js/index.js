435f0af12f4e3404941290a7ad9747ecfc66ccae
nec390 <50500824+nec390@users.noreply.github.com>
2019-06-02 02:43:10 -0700

    proyecto casi terminado

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
-var temporizador;
-var imagenes=[ "imagenes/ortodoncia2.jpg",
-              "imagenes/ortodoncia3.jpg",
-              "imagenes/ortodoncia1.jpg"];
-var x=0;
-function cambioImagen(){
-  if(x > 2){
-    x=0;
-  }
-  document.getElementById('imagen2').src = imagenes[x];
-  x++
+// var temporizador;
+// var imagenes=[ "imagenes/ortodoncia2.jpg",
+//               "imagenes/ortodoncia3.jpg",
+//               "imagenes/ortodoncia1.jpg"];
+// var x=0;
+// function cambioImagen(){
+//   if(x > 2){
+//     x=0;
+//   }
+//   document.getElementById('imagen2').src = imagenes[x];
+//   x++
+// }
+//  temporizador = setInterval ( cambioImagen, 2000);
+function despeglarMenu(){
+  document.getElementById('menu1').classList.remove("menu_opc");
+  document.getElementById('menu1').classList.add("menu_visible");
 }
- temporizador = setInterval ( cambioImagen, 2000);
+function quitarMenu(){
+  document.getElementById('menu1').classList.remove("menu_visible");
+  document.getElementById('menu1').classList.add("menu_opc");
+}
